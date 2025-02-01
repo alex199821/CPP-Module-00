3fee63a083c82edf5fce20a6802f2968ada749ad
auplisas <aupli16@freeuni.edu.ge>
2025-02-01 02:57:26 +0100

    fix: CTRL+D and Empty Input Bugs

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,4 +1,4 @@
-# include "PhoneBook.hpp"
+#include "PhoneBook.hpp"
 
 // void	leaks(void)
 // {
@@ -9,21 +9,33 @@ void	handleSearch(PhoneBook &phoneBook)
 {
 	int	index;
 
-	if(phoneBook.getContactCount() > 0)
+	if (phoneBook.getContactCount() > 0)
 	{
 		phoneBook.displayContacts();
 		std::cout << "Enter index of contact to find: ";
-        if (std::cin >> index && index >= 0 && index <= phoneBook.getContactCount() - 1)
-        {
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            phoneBook.displaySingleContact(index);
-        }
-        else
-        {
-           std::cout << "\nError: index. Please enter a digit between 0 and "  << phoneBook.getContactCount() - 1 << ".\n\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
+		if (std::cin.peek() == '\n')
+		{
+			std::cout << RED << "\nError: Input cannot be empty. Please enter a valid index.\n\n" << RESET;
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			return;
+		}
+		if (!(std::cin >> index))
+		{
+			std::cout << std::endl;
+			exit(0) ;
+		}
+		if (index >= 0 && index <= phoneBook.getContactCount() - 1)
+		{
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			phoneBook.displaySingleContact(index);
+		}
+		else
+		{
+			std::cout << RED  <<  "\nError: Please enter a digit between 0 and " << phoneBook.getContactCount()
+				- 1 << ".\n\n" << RESET;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
 	}
 	else
 	{
@@ -32,30 +44,47 @@ void	handleSearch(PhoneBook &phoneBook)
 	}
 }
 
-void	handleAdd(PhoneBook &phoneBook)
-{
-	std::string firstName;
-	std::string lastName;
-	std::string nickName;
-	std::string phoneNumber;
-	std::cout << "First name: ";
-	std::cin >> firstName;
-	std::cout << "Last name: ";
-	std::cin >> lastName;
-	std::cout << "Nickname: ";
-	std::cin >> nickName;
-	std::cout << "Phone Number: ";
-	std::cin >> phoneNumber;
-	phoneBook.addContact(firstName, lastName, nickName, phoneNumber);
-	std::cin.ignore();
-	std::cout << GREEN  << "\nContact added\n\n" << RESET;
+bool getNonEmptyInput(const std::string& prompt, std::string& output) {
+    while (true) {
+        std::cout << prompt;
+        if (!std::getline(std::cin, output)) {
+            std::cout << std::endl;
+            return false;
+        }
+        
+        if (output.empty()) {
+            std::cout << RED << "Error: Input cannot be empty.\n" << RESET;
+        } else {
+            return true;
+        }
+    }
+}
+
+void handleAdd(PhoneBook &phoneBook) {
+    std::string firstName, lastName, nickName, phoneNumber;
+
+    if (!getNonEmptyInput("First name: ", firstName)) {
+        exit(0);
+    }
+    if (!getNonEmptyInput("Last name: ", lastName)) {
+        exit(0);
+    }
+    if (!getNonEmptyInput("Nickname: ", nickName)) {
+        exit(0);
+    }
+    if (!getNonEmptyInput("Phone Number: ", phoneNumber)) {
+        exit(0);
+    }
+
+    phoneBook.addContact(firstName, lastName, nickName, phoneNumber);
+    std::cout << GREEN << "Contact added\n" << RESET;
 }
 
 int	main(int argc, char **argv)
 {
-	// atexit(leaks);
 	PhoneBook	phoneBook;
 
+	// atexit(leaks);
 	(void)argc;
 	(void)argv;
 	std::string command;
@@ -64,11 +93,15 @@ int	main(int argc, char **argv)
 	std::cout << YELLOW << "You can use the following commands:" << RESET << std::endl;
 	std::cout << YELLOW << "ADD" << " - Add a new contact" << RESET << std::endl;
 	std::cout << YELLOW << "SEARCH" << " - Search for a contact" << RESET << std::endl;
-	std::cout << YELLOW << "EXIT" << " - Exit the application\n" << RESET << std::endl;
+	std::cout << YELLOW << "EXIT" << " - Exit the application\n"<< RESET << std::endl;
 	while (1)
 	{
 		std::cout << "Enter a Command: ";
 		std::getline(std::cin, command);
+		if (std::cin.eof())
+		{
+			break ;
+		}
 		if (command == "ADD")
 		{
 			handleAdd(phoneBook);
@@ -83,7 +116,7 @@ int	main(int argc, char **argv)
 		}
 		else
 		{
-			std::cout << RED << "\nInvalid Command. Please try again.\n\n" << RESET;
+			std::cout << RED << "Invalid Command. Please try again.\n" << RESET;
 		}
 	}
 	return (0);
